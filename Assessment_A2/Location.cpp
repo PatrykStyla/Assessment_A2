6427c0da27e4f252746a233be0296f94fa92b6fe
Patryk <patrkst22@gmail.com>
2020-02-15 22:02:10 +0000

    added std::map

--- Assessment_A2/Location.cpp
+++ Assessment_A2/Location.cpp
@@ -5,13 +5,13 @@ Location::Location()
 
 	// Initialize the values
 	m_sLocation =			{ "Florida", "Paris", "Milan", "Tokyo" };
-	m_fPrice =				{ 1300, 600, 950, 1800 };
+	m_fPrice =				{ 1300.0f, 600.0f, 950.0f, 1800.0f };
 	m_fMinimumTravellers =	{ 4, 2, 2, 4 };
 
 	m_sActivity =			{"Theme Park", "Guided Tours", "Sky Diving"};
-	m_fCostPerPerson =		{ 80, 35, 110 };
+	m_fCostPerPerson =		{ 80.0f, 35.0f, 110.0f };
 	m_sAvailableLocations =	
-	{ 
+    { 
 		{"Florida", "Paris" ,"Tokyo"},
 		{"Paris", "Milan" ,"Tokyo"},
 		{"Florida"}
@@ -24,4 +24,43 @@ Location::Location()
 		{"Guided Tours"},
         {"Theme Park", "Guided Tours" }
 	};
+
+    InitLocationPriceMap();
+    InitActivityPriceMap();
+    InitActivityLocationMap();
+    InitLocationActivityMap();
 }
+
+void Location::InitLocationPriceMap()
+{
+    for (size_t i = 0; i < m_sLocation.size(); i++)
+    {
+        map_LocationPrice.insert(std::pair<std::string, float>(m_sLocation[i], m_fPrice[i]));
+    }
+}
+
+void Location::InitActivityPriceMap()
+{
+    for (size_t i = 0; i < m_sActivity.size(); i++)
+    {
+        map_ActivityPrice.insert(std::pair<std::string, float>(m_sActivity[i], m_fCostPerPerson[i]));
+    }
+}
+
+void Location::InitActivityLocationMap()
+{
+    for (size_t i = 0; i < m_sActivity.size(); i++)
+    {
+        map_ActivityLocation.insert(std::pair<std::string, std::vector<std::string>>(m_sActivity[i], m_sAvailableLocations[i]));
+    }
+}
+
+void Location::InitLocationActivityMap()
+{
+    for (size_t i = 0; i < m_sLocation.size(); i++)
+    {
+        map_LocationActivity.insert(std::pair<std::string, std::vector<std::string>>(m_sLocation[i], m_sAvaliableActivities[i]));
+    }
+}
+
+
--- Assessment_A2/Location.h
+++ Assessment_A2/Location.h
@@ -2,11 +2,7 @@
 
 #include <vector>
 #include <string>
-
-enum class UserLocation
-{
-    Floria, Paris, Milan, Tokyo,
-};
+#include <map>
 
 class Location
 {
@@ -14,15 +10,28 @@ public:
 	// The locations the company has to offer
 	std::vector<std::string> m_sLocation;
 	std::vector<float> m_fPrice;
-	std::vector<float> m_fMinimumTravellers;
+	std::vector<int> m_fMinimumTravellers;
 
 	std::vector<std::string> m_sActivity;
 	std::vector<float> m_fCostPerPerson;
 	/* This lists the available locations based on the activity */
 	std::vector<std::vector<std::string>> m_sAvailableLocations;
 	/* This lists the available activities based on the location */
-	std::vector<std::vector<std::string>> m_sAvaliableActivities;
+    std::vector<std::vector<std::string>> m_sAvaliableActivities;
+
+
+	/* location => price */
+	std::map<std::string, float> map_LocationPrice;
+    /* location => available activities*/
+    std::map<std::string, std::vector<std::string>> map_ActivityLocation;
+	/* activity => price*/
+	std::map<std::string, float> map_ActivityPrice;
+	/* activity => available locations*/
+	std::map<std::string, std::vector<std::string>> map_LocationActivity;
 
 	Location();
+	void InitLocationPriceMap();
+	void InitActivityPriceMap();
+	void InitActivityLocationMap();
+	void InitLocationActivityMap();
 };
-
