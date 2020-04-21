30bb6e47294d6f35abaf8d82309d0706ce379df7
Patryk <patrykstyla22@gmail.com>
2020-04-21 22:13:00 +0100

    More helper functions

--- Assessment_A2/Helper.cpp
+++ Assessment_A2/Helper.cpp
@@ -2,24 +2,181 @@
 
 #include "Location.h"
 #include "Helper.h"
+#include <filesystem>
 
 void PrintLocations(Location& locations)
 {
-    for (size_t i = 0; i < locations.m_sLocation.size(); i++)
+    for (size_t i = 0; i <  locations.GetLocations().size(); i++)
     {
-        std::cout << i + 1 << ") Location: " << locations.m_sLocation[i]
-                           << ", Price: " << locations.m_fPrice[i] << char(156) << "\n";
+        std::cout << i + 1 << ") Location: " << locations.GetLocations().at(i).m_sLocation
+            << ", Price: " << char(156) << locations.GetLocations().at(i).m_fPrice << "\n";
     }
+    std::cout << "\n";
 }
 
-void PrintAttractions(Location& locations, int iChoiceDestination)
+void PrintLocationsDetailed(Location& locations)
 {
-    std::cout << "List of attractions at " << locations.m_sLocation[iChoiceDestination] << "\n";
-
-    for (size_t i = 0; i < locations.m_sAvaliableActivities[iChoiceDestination].size(); i++)
+    for (size_t i = 0; i < locations.GetLocations().size(); i++)
     {
-        std::cout << i + 1 << ") Attraction: " << locations.m_sAvaliableActivities[iChoiceDestination][i]
-                           << ", Price per user: " << locations.map_ActivityPrice.at(locations.m_sAvaliableActivities[iChoiceDestination][i])
-                           << char(156) << "\n";
+        std::cout << i + 1 << ") Location: " << locations.GetLocations().at(i).m_sLocation
+            << ", Price: " << char(156) << locations.GetLocations().at(i).m_fPrice
+            << ", Minimum Travelers: " << locations.GetLocations().at(i).m_iMinimumTravellers << ".\n";
+    }
+    std::cout << "\n";
+}
+
+void PrintDetailsForLocation(Location& locations, int iChoiceDestination)
+{
+    std::cout  << "Location: " << locations.GetLocations().at(iChoiceDestination).m_sLocation
+        << ", Price: " << char(156) << locations.GetLocations().at(iChoiceDestination).m_fPrice
+        << ", Minimum Travelers: " << locations.GetLocations().at(iChoiceDestination).m_iMinimumTravellers << ".\n";
+
+    std::cout << "\n";
+}
+
+void PrintAttractions (Location& locations, int iChoiceDestination)
+{
+    std::cout << "List of attractions at " << locations.GetLocations().at(iChoiceDestination).m_sLocation << "\n\n";
+
+    for (size_t i = 0; i < locations.GetLocations().at(iChoiceDestination).Activities.size(); i++)
+    {
+        std::cout << i + 1 << ") Attraction: " << locations.GetLocations().at(iChoiceDestination).Activities.at(i)->m_sActivity
+            << ", Price per user: " << char(156) << locations.GetLocations().at(iChoiceDestination).Activities.at(i)->m_fCostPerPerson << "\n";
+    }
+    std::cout << "\n";
+}
+
+void PrintAllAttractions(Location& locations)
+{
+    for (size_t i = 0; i < locations.GetActivities().size(); i++)
+    {
+        std::cout << i + 1 << ") Attraction: " << locations.GetActivities().at(i)->m_sActivity
+            << ", Price per user: " << char(156) << locations.GetActivities().at(i)->m_fCostPerPerson << "\n";
+    }
+    std::cout << "\n";
+}
+
+void PrintListFamilies()
+{
+    std::cout << "List of all Families.\n\n";
+    // https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
+    std::string path = "jsons/";
+    std::string sToRemove[3] = { "\"", "jsons/", ".json" };
+    int i = 0;
+    for (auto entry : std::filesystem::directory_iterator(path))
+    {
+        // loop 3 times to remove "", jsons/ & .json
+        auto a = entry.path().string();
+        for (size_t i = 0; i < 3; i++)
+        {
+            ReplaceStringInPlace(a, sToRemove[i], "");
+        }
+
+        std::cout << i + 1 << ") " << a << "\n";
+        i++;
+    }
+    std::cout << "\n";
+}
+
+bool RegexYesNo(bool& bMore, std::string& sChoice)
+{
+    if (std::regex_match(sChoice, std::regex("[y](?:es)?", std::regex_constants::icase)))
+    {
+        bMore = true;
+    }
+    else if (std::regex_match(sChoice, std::regex("[n](?:o)?", std::regex_constants::icase)))
+    {
+        bMore = false;
+    }
+    else
+    {
+        std::cout << "Please enter Yes(y) or No(n)\n";
+        return true;
+    }
+
+    return false;
+}
+
+int ValidateNumberInput()
+{
+    int iNumberToValidate;
+
+    std::cout << "\n";
+    std::cin >> iNumberToValidate;
+    std::cout << "\n";
+
+    // if cin fails (not a number)
+    while (!std::cin)
+    {
+        // https://stackoverflow.com/questions/5655142/how-to-check-if-input-is-numeric-in-c
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        // end
+        std::cout << "Please enter a number\n";
+        std::cin >> iNumberToValidate;
+        std::cout << "\n";
+    }
+    std::cin.ignore();
+    return iNumberToValidate;
+}
+
+float ValidateFloatInput()
+{
+    float fNumberToValidate;
+
+    std::cout << "\n";
+    std::cin >> fNumberToValidate;
+    std::cout << "\n";
+    // if cin fails (not a number)
+    while (!std::cin)
+    {
+        // https://stackoverflow.com/questions/5655142/how-to-check-if-input-is-numeric-in-c
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        // end
+        std::cout << "Please enter a number\n";
+        std::cin >> fNumberToValidate;
+        std::cout << "\n";
+    }
+    std::cin.ignore();
+    return fNumberToValidate;
+}
+
+void AnotherLocation(bool& bExit)
+{
+    int iChoice;
+    bool bChoice = true; 
+    do
+    {
+        iChoice = ValidateNumberInput();
+
+        switch (iChoice)
+        {
+        case 0:
+            bExit = false;
+            bChoice = false;
+            break;
+        case 1:
+            bExit = true;
+            bChoice = false;
+            break;
+        default:
+            bChoice = true;
+            std::cout << "Please select a valid option.\n";
+            break;
+        }
+    } while (bChoice);
+}
+
+void GetStringInput(std::string& sString)
+{
+    getline(std::cin, sString);
+}
+
+void ReplaceStringInPlace(std::string& subject, const std::string& search, const std::string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos) {
+        subject.replace(pos, search.length(), replace);
+        pos += replace.length();
     }
 }
--- Assessment_A2/Helper.h
+++ Assessment_A2/Helper.h
@@ -1,5 +1,28 @@
 #pragma once
 #include "Location.h"
+#include <regex>
 
+// Prints all locations & price
 void PrintLocations(Location&);
+// Prints all locations & price & min travelers
+void PrintLocationsDetailed(Location&);
+// Print details for an individual location
+void PrintDetailsForLocation(Location&, int);
+// Print the attractions for a specific location & price
 void PrintAttractions(Location&, int);
+// Prints all attractions & price
+void PrintAllAttractions(Location&);
+// List the jsons file in the directory
+void PrintListFamilies();
+// validate yes/no. Will return true if the condition fails
+bool RegexYesNo(bool&, std::string&);
+// Will handle the input of a number, validate it and return the value when its correct
+int ValidateNumberInput();
+// Same but for floats
+float ValidateFloatInput();
+// used in admin for confirmation
+void AnotherLocation(bool&);
+// Get string with spaces
+void GetStringInput(std::string&);
+// https://stackoverflow.com/questions/5878775/how-to-find-and-replace-string
+void ReplaceStringInPlace(std::string& subject, const std::string& search, const std::string& replace);
